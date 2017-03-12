9ec7646a394265b78b66f9f298d805e28be55918
Antonio Skilton <antonio.skilton@little-bamboo.com>
2017-03-11 19:06:49 -0800

    victimAge value 998 also NA

--- R/clean.R
+++ R/clean.R
@@ -10,7 +10,7 @@ clean <- function(){
   df <- readr::read_csv("database.csv", progress = F)
   names(df) <- gsub(" ", "", names(df))
   df <- dplyr::mutate(df,
-                      VictimAge = ifelse((VictimRace == "Unknown" & VictimSex == "Unknown") | VictimAge == 99, NA, VictimAge),
+                      VictimAge = ifelse((VictimRace == "Unknown" & VictimSex == "Unknown") | VictimAge %in% c(99, 998), NA, VictimAge),
                       victim_unknown = ifelse(is.na(VictimAge), 1, 0),
                       PerpetratorAge = ifelse(PerpetratorAge == 0, NA, PerpetratorAge),
                       perpetrator_unknown = ifelse(is.na(PerpetratorAge), 1, 0))
